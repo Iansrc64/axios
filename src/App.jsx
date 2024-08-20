9c48a5cbff1d24c004a34ff2592aebeab6219765
Visitante Digital <visitante.digital@DIGITALCOLLEGE.LOCAL>
2024-08-20 10:33:29 -0300

    Começando projeto com vite

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  useEffect(() => {
+    console.log('hello world')
+
+    //requisição
+  }) 
+
+  return <div>teste</div>
+}
+
+export default App
